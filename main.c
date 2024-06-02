c0f7201f899b1993b4ff51ddfb2f64bd43b3972d
Serdar Akcay <serdar.akcay@alumni.fh-aachen.de>
2024-06-02 12:49:46 +0200

    Functioning state right here!

--- main.c
+++ main.c
@@ -1,171 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include "resources.h"
+#include <unistd.h>
 #include <raylib.h>
 
+#include "map_src_list.h"
+#include "resources.h"
 
-/*while line_obj, line_src:
-	if strcmp(line_obj, line_src):
-		
-	
-		build_obj += line_obj
-		continue
-
-	MapColor mapc = {
-		.obj = build_obj,
-		.src = line_src,
-		.color = RED
-	};
-*/	
-
-typedef struct {
-	char* src;
-	char* obj;
-	Color color;
-} MapColor;
-
-long last_position = 0L;
-
-char* remove_spaces(const char* s){
-	char* d = s;
-	char* tmp = (char*) malloc(strlen(d)+1);
-	strcpy(tmp, d);
-	int i = -1;
-	
-	do{
-		while (*d == ' ' || *d == '\t') {
-			d++;
-		}
-		i++;
-	}while(tmp[i] = *d++);
-	return tmp; 
+char* copy_string(char *s){
+	char* s2;
+	s2 = (char*) malloc(strlen(s)+1);
+	strcpy(s2, s);
+	return (char*)s2;
 }
 
-/*
-TODO: Try to set fseek for the last possition of obj_line that way
-we dont go all the way back to the start but we stay in the context of the current thing (function, while, for, etc.)
-otherwise the brackets are making everything weird and break
-*/
-MapColor get_obj_block(char* obj_file, char* line, char* next_line){
-	FILE *obj_fp;
-	char *obj_line = NULL;
-	size_t obj_len = 0;
-	ssize_t obj_read;
+void block_until_next(FILE *fp, MapSrc* node, long last_pos, char* src_filename){
+	char *asm_line = NULL;
+	size_t asm_len = 0;
+	ssize_t asm_read;
 
-	char obj_build[1024];
-	obj_build[0] = '\0';
+	fseek(fp, last_pos, SEEK_SET);
 
-	obj_fp = fopen(obj_file, "r");
-	
-	char* obj_line_clean;
-	char* line_clean = remove_spaces(line);
-	char* next_line_clean = remove_spaces(next_line);
-	
-	fseek(obj_fp, last_position, SEEK_SET);
-
-	while ((obj_read = getline(&obj_line, &obj_len, obj_fp)) != -1) {
-		obj_line_clean = remove_spaces(obj_line);
-		if (strstr(obj_line_clean, line_clean) == NULL){ 
-			continue;
+	char* commentline = "";
+	asprintf(&commentline, "# %s:", src_filename);
+	int i = 0;
+	char* tmp;
+	while ((asm_read = getline(&asm_line, &asm_len, fp)) != -1) {
+		if( (tmp =  strstr(asm_line, ".globl")) != NULL ){
+			//node->src_line = tmp;
+			//node->src_line[0] = '\n'; 
+			break;
 		}
-			
-		//printf("BLOCK: %s", line);
-		printf("SRC: %s, OBJ: %s\n", line, obj_line);
-
-		if(strlen(next_line) == 0){
-			while ((obj_read = getline(&obj_line, &obj_len, obj_fp)) != -1){
-				strcat(obj_build, obj_line);
-				if(obj_line[0] == '\n'){
-					printf("BLOCK: %s\n", obj_build);
-					MapColor mpc = {
-						.src = next_line,
-						.obj = obj_build,
-						.color = RED
-					};
-
-					last_position = ftell(obj_fp);
-					obj_build[0] = '\0';
-					fclose(obj_fp);
-					return mpc;
-				} 
-			}
+		if(strstr(asm_line, commentline) != NULL){
+			break;
 		}
+		i++;
+		strcat(node->asm_block, asm_line);
+	}
 
-		last_position = ftell(obj_fp);
-		while ((obj_read = getline(&obj_line, &obj_len, obj_fp)) != -1){
-			obj_line_clean = remove_spaces(obj_line);
+	fseek(fp, last_pos, SEEK_SET);
+	node->asm_line_count = i;	
+}
+
+MapSrc* prepare_src_asm_mapping(char* asm_file, char* src_file){
+	FILE *asm_fp;
+	char *asm_line = NULL;
+	size_t asm_len = 0;
+	ssize_t asm_read;
+
+	asm_fp = fopen(asm_file, "r");
+
+	char* commentline = "";
+	asprintf(&commentline, "# %s:", src_file);
+
+	MapSrc* head_node = (MapSrc*) malloc(sizeof(MapSrc));
 	
-			if (strstr(obj_line_clean, next_line_clean) != NULL){
-				printf("BLOCK: %s\n", obj_build);
-				MapColor mpc = {
-					.src = next_line,
-					.obj = obj_build,
-					.color = RED
-				};
+	while ((asm_read = getline(&asm_line, &asm_len, asm_fp)) != -1) {
+		long last_pos = ftell(asm_fp);
 
-				obj_build[0] = '\0';
-				fclose(obj_fp);
-				return mpc;
-			}else{
-				strcat(obj_build, obj_line);
-			}
+		if(strstr(asm_line, commentline) != NULL){
+			char* inner_comment = strstr(asm_line, ":") + 1;
+			int line_num = atoi(inner_comment);
+		 	
+			MapSrc node = {
+				.filename = src_file,
+				.src_line = copy_string(strstr(asm_line,strstr(inner_comment, ":") + 1)),
+				.line_count = line_num,
+				.color = (Color){rand() % (255 + 1), rand() % (255 + 1), rand() % (255 +1), 255},
+			};
+			block_until_next(asm_fp, &node, last_pos, src_file);
 			
+			add_node(head_node, node);
 		}
 	}
 
-	fclose(obj_fp);
+	fclose(asm_fp);
+
+	return head_node;
 }
 
-void scan_obj(char* src_file, char* obj_file, MapColor *block){
-	FILE *src_fp;
-	char *src_line = NULL;
-	size_t src_len = 0;
-	ssize_t src_read;
-
-	src_fp = fopen(src_file, "r");
-
-	size_t i = 0;
-	long pos = NULL;
-
-	while ((src_read = getline(&src_line, &src_len, src_fp)) != -1) {
-		if (src_line[0] == '\n'){ continue; }
-
-		char* line = src_line;
-		char* next_line = NULL;
-		fflush(src_fp);
-		pos = ftell(src_fp);
-
-		getline(&next_line, &src_len, src_fp);
-		fseek(src_fp, pos, SEEK_SET);
-		
-		block[i] = get_obj_block(obj_file, line, next_line);
-		i++;
-	}
-
-	fclose(src_fp);
-}
-
-char* create_buid_objdump(char* src_file){
-	const char *cmd_build = "/bin/gcc";
+void create_buid_asm(char* src_file){
+	const char *cmd_build_asm = "/bin/gcc";
 	
-	char bin_file[100], file_end[100];
-	strcpy(bin_file, src_file);
-	strcpy(file_end, ".bin");
-	strcat(bin_file, file_end);
+	char asm_file[100], file_end[100];
+	strcpy(asm_file, src_file);
+	strcpy(file_end, ".s");
+	strcat(asm_file, file_end);
 
-	const char *args_build[] = {"gcc", "-g", src_file, "-o", bin_file, NULL};
-	
-	const char *cmd_obj = "bin/objdump";
-	const char *args_obj[] = {"objdump", "-S", bin_file, "-M", "intel", ">", "test.txt", NULL};
+	char * const args[] = {"gcc", "-S", "-Iincludes","-fverbose-asm", "-masm=intel", src_file, "-o", asm_file, NULL};
+	execv(cmd_build_asm, args);
 
-	execv(cmd_build, args_build);
-	execv(cmd_obj, args_obj);
-
-	return bin_file;
 }
 
+// Weird memory leak with this
 void setup_line_numbers(Vector2 pos){
 	int text_width_last = 0;
  	int text_width_current = 0;
@@ -177,7 +104,7 @@ void setup_line_numbers(Vector2 pos){
 	DrawText(editor_lines, 0, 0, FONT_SIZE, WHITE);
 }
 
-void text_area(Vector2 pos, Vector2 size, Color color, float border_thickness, char filepath[]){
+void ftext_area(Vector2 pos, Vector2 size, Color color, float border_thickness, char filepath[]){
 	char text_buffer[(int)size.x/FONT_SIZE];
 	char* filedata = LoadFileText(filepath);
 
@@ -195,34 +122,114 @@ void text_area(Vector2 pos, Vector2 size, Color color, float border_thickness, c
 	DrawText(filedata, pos.x, pos.y, FONT_SIZE, color);
 }
 
+void text_area(Vector2 pos, Vector2 size, Color color, float border_thickness, char* txt){
+	char text_buffer[(int)size.x/FONT_SIZE];
+
+	Rectangle text_area_border = {
+			.x = pos.x,
+			.y = pos.y,
+			.width = size.x,
+			.height = size.y
+	};
+
+	// Weird memory leak with this
+	//setup_line_numbers((Vector2){text_area_border.x +5 , text_area_border.y+5});
+ 	
+	DrawRectangleLinesEx(text_area_border, border_thickness, color);
+	DrawText(txt, pos.x, pos.y, FONT_SIZE, color);
+}
+
+int line_count(char* str){
+	char* tmp = str;
+	int i = 0;
+	
+	if(strlen(str) == 0){
+		return 0;
+	}
+
+	while( (tmp = strstr(tmp, "\n") + 1) != NULL ){
+		i++;
+	}
+
+	return i;
+}
+
 int main(){
 
-	//create_buid_objdump("example/square.c");
-	
-	MapColor *mpc = (MapColor*) malloc(sizeof(MapColor) * 1024);
-	scan_obj("example/square.c", "example/square.c.s", mpc);
-	printf("\n\nSOURCE: %s, OBJECT: %s\n\n", mpc[5].src, mpc[5].obj);	
-	free(mpc);
-	return 0;
+	char* src_file = "main.c";
+	char* asm_file = "main.c.s";
+
+	MapSrc* ms = prepare_src_asm_mapping(asm_file, src_file);
+	MapSrc* tmp_ms = ms;
 
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE_TEXT);
+	ClearBackground(BLACK);
 	
+	Camera2D camera =  { 0 };
+	camera.target = (Vector2) {SCREEN_WIDTH, SCREEN_HEIGHT};
+	camera.offset = (Vector2) {SCREEN_WIDTH, SCREEN_HEIGHT};
+	camera.rotation = 0.0f;
+	camera.zoom = 1.0f;
 
 	while(!WindowShouldClose()){
 		BeginDrawing();
+		ClearBackground(BLACK);
+		int i = 0;
+		int j = 0;
+		float asm_block_pos = 5, drift = 0.0;
+		float src_line_pos = 5;
+	
+		Vector2 text_scroll_offset = { 0 };
 
+		if (IsKeyDown(KEY_UP)) text_scroll_offset.y -= 5.0;
+		else if (IsKeyDown(KEY_DOWN)) text_scroll_offset.y += 5.0;
+
+		camera.target = (Vector2){
+			camera.target.x + text_scroll_offset.x,
+			camera.target.y + text_scroll_offset.y
+		};
+	
+		BeginMode2D(camera);
+		while(tmp_ms != NULL){
+			src_line_pos = i*(FONT_SIZE + 5);
+
+			text_area(
+				(Vector2){0,src_line_pos},
+				(Vector2){SCREEN_HEIGHT, SCREEN_WIDTH/2},
+				tmp_ms->color,
+				0.0,
+				tmp_ms->src_line
+			);	
+			
+			if(tmp_ms->parent != NULL){
+				asm_block_pos += ((FONT_SIZE + 5) * tmp_ms->parent->asm_line_count) + 5;
+			}
+
+			text_area(
+				(Vector2){SCREEN_WIDTH/2, asm_block_pos},
+				(Vector2){SCREEN_HEIGHT, SCREEN_WIDTH},
+				tmp_ms->color,
+				0.0,
+				tmp_ms->asm_block
+			);	
+		
+			i++;
+			j++;	
+			tmp_ms = tmp_ms->child;
+		}
+		EndMode2D();
+
+		free(tmp_ms);
+		tmp_ms = ms;
+		
 		DrawLineV(
 			(Vector2){ SCREEN_WIDTH/2, 0 },
 			(Vector2){ SCREEN_WIDTH/2, SCREEN_HEIGHT },
 			WHITE
 		);
-
-	
-		text_area((Vector2){20, 0}, (Vector2){SCREEN_WIDTH/2 -20, SCREEN_HEIGHT}, BLUE, 0.0f, "example/square.c");
-		text_area((Vector2){SCREEN_WIDTH/2, 0}, (Vector2){SCREEN_WIDTH, SCREEN_HEIGHT}, BLUE, 0.0f, "example/square.c.s");
-
 		EndDrawing();
 	}
-	
+
 	return 0;
+ 
 }
